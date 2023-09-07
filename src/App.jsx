7511aa1b3b3301f847065d073b6bd60aaf85e442
unknown <ahnaf.ahmed.42173@gmail.com>
2023-09-07 23:21:05 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    alert('clicked')    
+  }
+  const handleClick2 = () =>{
+    alert('clicked')
+  }
+  const handleClick3 = (num) =>{
+    alert('Number is: '+num)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>click here</button>
+      <button onClick={handleClick2}>click here 2</button>
+      <button onClick={() =>{alert('clicked')}}>click here 3</button>
+      <button onClick={() => handleClick3(5)}>click here 4</button>
     </>
   )
 }
