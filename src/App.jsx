f464b901249fe42c2d3b8e00059eaf948f705862
unknown <ahnaf.ahmed.42173@gmail.com>
2023-09-08 22:01:43 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import Users from './Users'
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
 
   return (
     <>
-
+    <Users></Users>
     <Counter></Counter>
 
       <h1>Vite + React</h1>
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react"
+import User from "./user"
 
 export default function Users(){
 
     const [user , setUser] = useState([])
     useEffect(() =>{
-        
+         fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUser(data))
+
     } ,[])
 
     return(
         <div>
-
+            <h3>Users: {user.length}</h3>
+            {
+                user.map(user => <User user={user}></User>)
+            }
         </div>
     )
 }
